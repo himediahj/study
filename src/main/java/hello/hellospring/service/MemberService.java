25a6e990b2b4977f9747145974d5d02d58a87a43
temisonehj <himedia.hj@gmail.com>
2023-03-27 17:15:24 +0900

    spring 입문

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,43 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {    // ctrl shift t -> test 자동으로 만들어줌
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member){
+        /*Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });*/
+
+        validateDuplicateMember(member);    // shift ctrl alt t (refactor 관련 단축 키 -> 외부 메서드 추출)
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
